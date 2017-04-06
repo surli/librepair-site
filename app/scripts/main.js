ddb9f8e0e79054764e06c77df6041ef8ab288b20
Simon Urli <simon.urli@inria.fr>
2017-04-06 14:08:36 +0200

    Init commit

---
+++ app/scripts/main.js
@@ -0,0 +1,40 @@
+$.get('http://localhost:4040/api/inspectors/hostnameStats', function (data) {
+  console.log(data);
+  var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+
+  var color = d3.scale.ordinal()
+    .range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
+
+  var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+  var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.counted; });
+
+  var svg = d3.select('#charts').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width-radius) / 2 + ',' + height / 2 + ')');
+
+  var g = svg.selectAll('.arc')
+    .data(pie(data))
+    .enter().append('g')
+    .attr('class', 'arc');
+
+  g.append('path')
+    .attr('d', arc)
+    .style('fill', function(d) { return color(d.data._id); });
+
+  g.append('text')
+    .attr('transform', function(d) { return 'translate(' + arc.centroid(d) + ')'; })
+    .attr('dy', '.35em')
+    .text(function(d) { return d.data._id; });
+
+
+});
